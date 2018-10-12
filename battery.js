cc3e7e7c33b1e3fffdf0010fa3cec2d96f8d9080
Grant Judkins <gjudkins@snapfinance.com>
2018-10-11 23:46:20 -0600

    added timestamps and logging

--- battery.js
+++ battery.js
@@ -1,6 +1,8 @@
 (function(){
     const fetch = require("isomorphic-fetch");
+    const moment = require('moment');
     const config = require('./config.json')[0];
+    const fs = require('fs');
 
     const key = [
         ['warn', '\x1b[35m', '(?)'],
@@ -8,8 +10,13 @@
         ['log', '\x1b[32m', 'UP']
     ]
 
+    const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+
     // URLS that will be checked
     var urls = [];
+    var output = '';
+    var urlCount = 0;
+    var urlInc = 0;
 
     // Additional static URLS from config
     if(config.urls){
@@ -44,6 +51,7 @@
             // Fitler results
             if(filter){
                 urls = urls.filter(function (str) { return str.indexOf(filter) !== -1; });
+                
             }
             
             urls.forEach(env => {
@@ -52,30 +60,63 @@
             
         },
         run: function(){
+            console.info('Started at '+currentTime);
+            urlCount = urls.length;
+            console.log('URLS LENGTH', urls.length);
             engine.check(false);
         },
         // Gets the status of a url
         getUrlStatus: function(env) {
             var p = new Promise(function (resolve, reject) {
+                var message = '';
                 fetch(env).then((response) => {
                     if (response.statusText && response.statusText == 'OK') {
                         engine.lineBreak();
-                        console.log(env + ' (' + response.status + ')');
+                        message = '[UP] ' + env + ' (' + response.status + ') | ' + moment().format('MM-DD-YY, h:mm:ss');
+                        console.log(message);
+                        output += message;
                         resolve(); // fulfilled successfully
                     } else {
                         engine.lineBreak();
-                        console.error(env + ' (' + response.status + ')');
+                        message = '[DOWN] ' + env + ' (' + response.status + ') | ' + moment().format('MM-DD-YY, h:mm:ss');
+                        console.error(message);
+                        output += message + '\n';
                         reject(); // error, rejected
                     }
+                    urlInc++;                    
+
+                    // Log output when last promise has been fulfilled
+                    //if ((urlInc === urlLength) && (config.logging === true)) {
+                        engine.logOutput();
+                    //}
+                    
                 }).catch((error) => {
                     engine.lineBreak();
-                    console.warn('\x1b[36m%s\x1b[0m', env + ' is not valid! (' + error.status + ')');
+                    message = '[?] ' + env + ' is not valid! (' + error.status + ') | ' + moment().format('MM-DD-YY, h:mm:ss');
+                    console.warn(message);
+                    output += message + '\n';
+
+                    urlInc++;                    
+
+                    engine.logOutput();
+
                     reject(error);
+
                 });
 
                 resolve();
 
             });
+        },
+        logOutput: function () {
+            if ((urlInc === urlCount) && (config.logging === true)) {
+                var name = 'logs/battery-log_' + moment().format('MM-DD-YY') + '_' + moment().format('h:mm:ss a') + '.txt';
+                fs.appendFile(name, output, function (err) {
+                    if (err) throw err;
+                    engine.lineBreak();
+                    console.info('Saved to log: ' + name);
+                });
+            }
         },
         // To make it... prettyish? 
         lineBreak: function () {console.info('-----------------------------------------------------------------------');}
